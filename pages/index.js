d18799a8f32850249b4ffc6ae617bc26727a4b35
Dan Tran <dantranux@gmail.com>
2022-01-13 19:55:28 -0800

    Added profile picture to home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
-        Hello, I&apos;m a full-stack developer based in California
+      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+        Hello, I&apos;m a Full Stack Developer & UX Designer based in California
       </Box>
 
       <Box display={{ md: 'flex' }}>
@@ -12,7 +12,19 @@ const Page = () => {
           <Heading as="h2" variant="page-title">
             Dan Tran
           </Heading>
-          <p>Tagline (Title)</p>
+          <p>Full Stack Developer & UX Designer</p>
+        </Box>
+        <Box flexShrink={0} mt={{base:4, md: 0}} ml={{md: 6}} align="center">
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/profile.jpg"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
