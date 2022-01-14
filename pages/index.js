a7f48232c7648e51172c247916bc777bd21864f4
Dan Tran <dantranux@gmail.com>
2022-01-13 16:37:41 -0800

    Added Navbar

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer based in California
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Dan Tran
+          </Heading>
+          <p>Tagline (Title)</p>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default Page;
