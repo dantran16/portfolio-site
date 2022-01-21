2eea350e3aca6fdde23525fe74e59b006399cdd5
dantran16 <dantranux@gmail.com>
2022-01-20 19:01:26 -0800

    added a 404 page

--- pages/index.js
+++ pages/index.js
@@ -3,66 +3,69 @@ import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import NextLink from 'next/link'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
   return (
-    <Container maxW='container.md'>
-      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
-        Hello, I&apos;m a Full Stack Developer & UX Designer based in California!
-      </Box>
+    <Layout>
+      <Container maxW='container.md'>
+        <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+          Hello, I&apos;m a Full Stack Developer & UX Designer based in California!
+        </Box>
 
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Dan Tran
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Dan Tran
+            </Heading>
+            <p>Full Stack Developer & UX Designer</p>
+          </Box>
+          <Box flexShrink={0} mt={{base:4, md: 0}} mx={{md: 100}} align="center" >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="150px"
+              display="inline-block"
+              src="/images/profile.jpg"
+              alt="Profile Image"
+            />
+          </Box>
+        </Box>
+
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Projects
           </Heading>
-          <p>Full Stack Developer & UX Designer</p>
-        </Box>
-        <Box flexShrink={0} mt={{base:4, md: 0}} mx={{md: 100}} align="center" >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="150px"
-            display="inline-block"
-            src="/images/profile.jpg"
-            alt="Profile Image"
-          />
-        </Box>
-      </Box>
+          
+        </Section>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Projects
-        </Heading>
-        
-      </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            About Me
+          </Heading>
+          <Paragraph>I am an adaptable full stack developer and UX designer based in California. I am passionate in improving the experience of people. With my curiosity to learn, I am confident in my ability to create the best experience for users.
+          </Paragraph>
+          <Box align="left" my={4} size="lg">
+            <NextLink href="/about">
+              <Button isFullWidth={true} rightIcon={<ChevronRightIcon />} colorScheme="teal" variant='outline'>Learn more</Button> 
+            </NextLink>
+          </Box>
+        </Section>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          About Me
-        </Heading>
-        <Paragraph>I am an adaptable full stack developer and UX designer based in California. I am passionate in improving the experience of people. With my curiosity to learn, I am confident in my ability to create the best experience for users.
-        </Paragraph>
-        <Box align="left" my={4} size="lg">
-          <NextLink href="/about">
-            <Button isFullWidth={true} rightIcon={<ChevronRightIcon />} colorScheme="teal" variant='outline'>Learn more</Button> 
-          </NextLink>
-        </Box>
-      </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Skills
+          </Heading>
+        </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          Skills
-        </Heading>
-      </Section>
-
-      <Section delay={0.4}>
-        <Heading as="h3" variant="section-title">
-          Links
-        </Heading>
-      </Section>
-    </Container>
+        <Section delay={0.4}>
+          <Heading as="h3" variant="section-title">
+            Links
+          </Heading>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,30 +1,33 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { ProjectGridItem } from '../components/GridItem'
+import Layout from '../components/layouts/article'
 
 import thumbTemporal from '../public/images/projects/thumb_temporal.png'
 import thumbPinterest from '../public/images/projects/thumb_pinterest.jpg'
 
 const Projects = () => {
   return (
-    <Container maxW='container.md'>
-      <Heading as="h3" fontSize={28} mb={4}>
-        Projects
-      </Heading>
+    <Layout title="Projects">
+      <Container maxW='container.md'>
+        <Heading as="h3" fontSize={28} mb={4}>
+          Projects
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <ProjectGridItem id="temporal" title="Temporal" thumbnail={thumbTemporal}>
-            A task managing app that uses NodeJS, Express, and MongoDB.
-          </ProjectGridItem>
-        </Section>
-        <Section>
-          <ProjectGridItem id="pinterest" title="Pinterest Component Redesign" thumbnail={thumbPinterest}>
-            A school project that offers an alternative approach to Pinterest's design on its search component in 2020. Involves user testing and UX Design.
-          </ProjectGridItem>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <ProjectGridItem id="temporal" title="Temporal" thumbnail={thumbTemporal}>
+              A task managing app that uses NodeJS, Express, and MongoDB.
+            </ProjectGridItem>
           </Section>
-      </SimpleGrid>
-    </Container>
+          <Section>
+            <ProjectGridItem id="pinterest" title="Pinterest Component Redesign" thumbnail={thumbPinterest}>
+              A school project that offers an alternative approach to Pinterest's design on its search component in 2020. Involves user testing and UX Design.
+            </ProjectGridItem>
+            </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 export default Projects
